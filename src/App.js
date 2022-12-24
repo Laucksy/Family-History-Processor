ff0837200b886df7b0189f395729b34e4454bab6
Erik Laucks <eriklaucks@gmail.com>
2022-12-24 12:26:23 -0500

    Main UI updates

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
         <h5 className="text-light">Laucks Family History</h5>
       </div>
 
-      <div className="p-5 bg-light text-center">
+      <div className="p-4 bg-light text-center">
         <div className="container">
-          <h1 className="display-4">Welcome to the Family History Processor!</h1>
-          <p className="lead text-muted">This is a tool for documenting family members and creating family trees.</p>
+          <h1 className="display-6">Welcome to the Family History Processor!</h1>
+          <p className="lead text-muted">This is a tool for creating family trees.</p>
         </div>
       </div>
 
